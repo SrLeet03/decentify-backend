fa475fb933163f805dbe15cfeb343d4bcaf7570a
root <root@LAPTOP-CBECH9G3.localdomain>
2021-10-25 01:17:31 +0530

    contracts modf

--- contracts/Decentify.sol
+++ contracts/Decentify.sol
@@ -1,11 +1,33 @@
 
-pragma solidity ^0.4.16;
+pragma solidity 0.4.17;
 
 
 contract Decentify {
 
-    address  public owner; 
-    int64    public minContri;
+    struct Requests {
 
-     
+        string description;
+        uint value;
+        address recepient;
+        bool isComplete;
+
+    }
+
+    address  public manager; 
+
+    uint    public minContri;
+    
+    address[] public doners;
+
+    function Decentify(uint argforMIN) public {
+        manager = msg.sender;
+        minContri = argforMIN;
+    }
+
+    function donate() public payable {
+
+        require(msg.value > minContri);
+        doners.push(msg.sender);
+    }
+ 
 }
