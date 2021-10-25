5b6231049d4dc742655f8bccea7891197f2c45ba
root <root@LAPTOP-CBECH9G3.localdomain>
2021-10-25 13:52:32 +0530

    contracts : craeted the fundriase factor which will keep tracck of all the deployed fundraisers / contracts and compililation script added

--- compile.js
+++ compile.js
@@ -5,6 +5,7 @@ const fs = require('fs-extra') ;
 
 const buildPath = path.resolve(__dirname , 'build') ; 
 
+
 fs.removeSync(buildPath) ; 
 
 const contractPath = path.resolve(__dirname , 'contracts' , 'Decentify.sol' ) ; 
