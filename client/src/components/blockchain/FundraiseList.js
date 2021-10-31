b6290178119c33c119487f2a0cbc63e86038a8ce
root <root@LAPTOP-CBECH9G3.localdomain>
2021-10-31 15:02:16 +0530

    get all fundrs front end done

--- client/src/components/blockchain/FundraiseList.js
+++ client/src/components/blockchain/FundraiseList.js
@@ -1,72 +1,95 @@
-import React, { useState , useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
-import axois from 'axios' ; 
-import { useSelector  , useDispatch } from 'react-redux';
-import { Row, Button } from 'react-bootstrap';
-
+import axois from 'axios';
+import { useSelector, useDispatch } from 'react-redux';
+import { Row, Button, Badge , Navbar , Container } from 'react-bootstrap';
+import { URL } from '../../helper/helper';
 import FundCard from './FundCard'
 import axios from 'axios';
+import { setFundrs } from '../../redux/actions/action';
 
-//import './css/card.css'
-
-const arr = [1, 1, 1, 1, 1, 1, 1];
 
 
 
 export default function FundraiseList() {
 
-    const [tag, setTag] = useState("");
-    const dispatch = useDispatch() ;  
-    useEffect (() =>{
-        
-        const data = axios.get(`${process.env.URL}/getallFundrs`)
-        .then((result) =>{
-            console.log(result) ; 
-        }).catch((err) => {
-            console.log(err) ; 
-        })
+    const [tag, setTag] = useState("all categories");
+    const [allf, setAllf] = useState([{}]);
+    const dispatch = useDispatch();
+    useEffect(() => {
 
-    } , [tag]) ; 
+
+
+        axios.get(`${URL}/getpost/${tag}`)
+            .then((result) => {
+                setAllf(result.data);
+                dispatch(setFundrs(result.data));
+            }).catch((err) => {
+                console.log(err);
+            })
+
+    }, []);
     const handleselect = (arg) => {
         setTag(arg);
+        if (arg === "all categories") {
+            setAllf(fundrlist);
+            return;
+        }
+        const newFundarr = fundrlist.filter((value) => {
+
+            return (value?.tag === arg);
+        });
+        setAllf(newFundarr);
+
     }
-     const fundrlist = useSelector((state) => state) ; 
-     console.log("check" , fundrlist) ; 
+    const fundrlist = useSelector((state) => state.allFundrs.fundrs);
+
+
     return (
         <div>
-            all fundraisers
+
             <div className="tags">
-                <b>select tag to donate your prefered category fundraiser : </b>
-                <Button onClick = {() =>handleselect("all categories")} variant="info">All categories</Button>{'   '}
-                <Button onClick ={() =>handleselect("medical")}variant="info">Medical</Button>{'   '}
+                <Navbar sticky="top" expand="sm" bg="dark" variant="dark">
+                    <Container>
+                        <Navbar.Brand>Select tag to donate your preferred category fundraiser </Navbar.Brand>
+                    </Container>
+                </Navbar>
+               
+                <Button onClick={() => handleselect("all categories")} variant="info">All Categories</Button>{'   '}
+                <Button onClick={() => handleselect("medical")} variant="info">Medical</Button>{'   '}
 
-                <Button onClick={() =>handleselect("enviroment")} variant="info">Enviroment</Button>{' '}
+                <Button onClick={() => handleselect("enviroment")} variant="info">Enviroment</Button>{' '}
 
-                <Button onClick={() =>handleselect("education")} variant="info">Education</Button>{' '}
+                <Button onClick={() => handleselect("education")} variant="info">Education</Button>{' '}
 
+                <Button onClick={() => handleselect("health")} variant="info">Health</Button>{' '}
 
-                <Button onClick={() =>handleselect("social service")} variant="info">Social Service</Button>{' '}
+                <Button onClick={() => handleselect("social service")} variant="info">Social Service</Button>{' '}
                 <br></br>
                 {
-                    tag!==""?<b>{tag} Fundraisers : </b> : ""
+                    tag !== "" ? <b> <Badge pill bg="dark">
+                        {tag}
+                    </Badge> </b> : ""
                 }
 
             </div>
-           
+
             <br></br>
             <br></br> <br></br>
+            {allf.length === 0 ?
+                <h1>Soory,you can diffenrent catg to donate caz this one has no fundr</h1> : ""}
             <div className="justify-content-md-center">
                 <Row class="row ">
                     {
-                        arr.map((key, value) => {
+                        allf.map((key, value) => {
                             const prop = {
-                                name  : 'sarvesh raut',
-                                title : "Helps us please!",
-                                date : '10 Nov,2019',
-                                tagline : "Some quick example text to build on the card title and make up the bulk of the card's"
+                                name: 'sarvesh raut',
+                                title: "Helps us please!",
+                                date: '10 Nov,2019',
+                                tagline: "Some quick example text to build on the card title and make up the bulk of the card's"
                             }
-                            return <FundCard  info = {prop} />;
+                            return <FundCard info={prop} />;
                         })
                     }
                 </Row>
